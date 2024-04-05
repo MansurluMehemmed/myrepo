827c6d9eda64581b0e89509ccaaf0602b9c0313e
Mehemmed <LENOVA-NB@LENOVA>
2024-04-05 13:30:04 +0400

    added

---
+++ main.js
@@ -0,0 +1,73 @@
+1
+const sentence = ['sense', 'make', 'all', 'will', 'This'];
+function reverseArray(arr) {
+    return arr.reverse();
+}
+console.log(reverseArray(sentence))
+2
+const greetAliens = function(aliens){
+    aliens.forEach(name => {
+      console.log('Oh powerful ${name}, we humans offer our unconditional surrender!')
+    });
+  
+  }
+  const aliens = ['Blorgous', 'Glamyx', 'Wegord','SpaceKing'];
+  greetAliens(aliens);
+  
+  
+  
+
+3
+const coolStuff = [
+    'gameboys',
+    'skateboards',
+    'backwards hats',
+    'my room',
+    'temporary tattoos'
+  ];
+  const myStuff = [
+    'sweaters',
+    'skateboards',
+    'family-night',
+    'my room',
+  ];
+  let a=[];
+  function justCoolStuff(arr1,arr2){
+  
+      for(let i=0;i<arr1.length;i++){
+           for(let j=0;j<arr2.length;j++){
+          if(arr1[i]===arr2[j]){
+              a.push(arr2[j]);
+          }
+      }
+      }
+      return a;
+  }
+  console.log(justCoolStuff(myStuff, coolStuff));
+////////
+4
+  const meal = [
+    { name: 'arugula', source: 'plant' },
+    { name: 'tomatoes', source: 'plant' },
+    { name: 'lemon', source: 'plant' },
+    { name: 'olive oil', source: 'plant' },
+  ];
+  const dinner = [
+    { name: 'hamburger', source: 'meat' },
+    { name: 'cheese', source: 'dairy' },
+    { name: 'ketchup', source: 'plant' },
+    { name: 'bun', source: 'plant' }
+  ];
+  
+  const isTheDinnerVegan  = function(food){
+        food.forEach(item=>{
+        // console.log(item.source)
+        if(item.source === 'plant'){
+          console.log(true)
+        }
+        else console.log(false)
+      })
+  }
+  
+  console.log(isTheDinnerVegan(meal));
+  console.log(isTheDinnerVegan(dinner));
